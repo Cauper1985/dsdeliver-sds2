6a04540747770ff7a3aee669a43d7bcf1adc4c78
André Cauper <andrecauper@yahoo.com.br>
2021-01-09 22:54:55 -0400

    Add routes and StepsHeader

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,14 +1,11 @@
+import { Route } from 'react-router-dom';
 import './App.css';
 import Hello from './Hello';
-import Home from './Home';
-import Navbar from './Navbar';
+import Routes from './Routes';
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Home />
-    </>
+       <Routes />
   );
 }
 
